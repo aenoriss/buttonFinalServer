101ccdf295c837ee019ce1ad622b41489fd92f36
aenoris <aenoriss@gmail.com>
2022-12-01 03:18:58 -0300

    zxczxc

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 const express = require("express");
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-const { Server } = require('ws');
+const INDEX = "/index.html";
+const { Server } = require("ws");
 
 let ledOn = false;
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-  
+
 const wss = new Server({ server });
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.on('message', function message(data) {
-        ledOn ? ledOn = false : ledOn = true; 
-        console.log("ledOn", ledOn);
-        wss.clients.forEach((client) => {
-            client.send(`${ledOn}`);
-          });
-      });
-    ws.on('close', () => console.log('Client disconnected'));
-  });
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  ws.send(`${ledOn}`);
 
+  ws.on("message", function message(data) {
+    ledOn ? (ledOn = false) : (ledOn = true);
+    console.log("ledOn", ledOn);
+    wss.clients.forEach((client) => {
+      client.send(`${ledOn}`);
+    })
+  
+    ws.on("close", () => console.log("Client disconnected"));
+});
+
+
+
+});
